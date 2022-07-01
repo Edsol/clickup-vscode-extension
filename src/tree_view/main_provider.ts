45c2a51fe4979fa440d72f369cc73aabde094092
Ignace Loomans <ignace.loomans@gmail.com>
2022-07-01 17:01:10 +0200

    Fix to fetch tasks

--- src/tree_view/main_provider.ts
+++ src/tree_view/main_provider.ts
@@ -58,8 +58,10 @@ export class MainProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
         if (element instanceof FolderItem) {
             var lists: Array<types.List> = await this.apiwrapper.getLists(element.folder.id);
             resolve = Object.values(lists).map(async (list: types.List) => {
-                return new ListItem(list, this.collapsedConst);
+                var taskCount = await this.apiwrapper.countTasks(list.id);
+                return new ListItem(list, this.collapsedConst,taskCount);
             });
+            resolve = await Promise.all(resolve)
         }
 
         if (element instanceof ListItem) {
